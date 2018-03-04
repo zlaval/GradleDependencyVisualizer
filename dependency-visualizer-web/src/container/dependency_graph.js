21e4f9c11c89a3a03866eedd8e996ba57fde0fe5
Zalan <zlaval@gmail.com>
2018-03-04 23:52:58 +0100

    middle tree

--- dependency-visualizer-web/src/container/dependency_graph.js
+++ dependency-visualizer-web/src/container/dependency_graph.js
@@ -4,10 +4,29 @@ import {listDependencies} from "../action/dependency_action"
 import Tree from 'react-d3-tree'
 
 
+const containerStyles = {
+    width: '100vw',
+    height: '100vh',
+}
+
 class DependencyGraph extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
 
     componentWillMount() {
         this.props.listDependencies()
+    }
+
+    componentDidMount() {
+        const dimensions = this.treeContainer.getBoundingClientRect();
+        this.setState({
+            translate: {
+                x: dimensions.width / 2,
+                y: dimensions.height / 2
+            }
+        })
     }
 
     render() {
@@ -25,9 +44,16 @@ class DependencyGraph extends Component {
             }
         }
 
+
         return (
-            <div id="treeWrapper" style={{width: '100em', height: '50em'}}>
-                <Tree data={[this.props.dependencies]} nodeSvgShape={svgSquare} pathFunc="diagonal" initialDepth="0" depthFactor={300}/>
+            <div style={containerStyles} ref={tc => (this.treeContainer = tc)}>
+                <Tree data={[this.props.dependencies]}
+                      nodeSvgShape={svgSquare}
+                      pathFunc="diagonal"
+                      initialDepth={0}
+                      depthFactor={300}
+                      translate={this.state.translate}
+                />
             </div>
         )
     }
