3da7377ff39127ac5c65a6ae4b47ded2b2d2b554
zalan <zlaval@gmail.com>
2018-03-10 13:48:21 +0100

    tooltip test

--- dependency-visualizer-web/src/container/dependency_graph.js
+++ dependency-visualizer-web/src/container/dependency_graph.js
@@ -2,17 +2,28 @@ import React, {Component} from "react"
 import {connect} from "react-redux";
 import Tree from 'react-d3-tree'
 import isEmptyObject from 'is-empty-object'
+import ToolTip from "react-portal-tooltip";
 
 
 const containerStyles = {
     width: '99vw',
     height: '95vh',
 }
+const tooltipStyles = {
+    height: '5vh',
+}
+
 
 class DependencyGraph extends Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            isTooltipActive: false,
+            toolTipText: ""
+        }
+        this.onMouseOver = this.onMouseOver.bind(this)
+        this.showTooltip = this.showTooltip.bind(this)
+        this.hideTooltip = this.hideTooltip.bind(this)
     }
 
     componentDidMount() {
@@ -23,6 +34,26 @@ class DependencyGraph extends Component {
                 y: dimensions.height / 2
             }
         })
+    }
+
+    onMouseOver(node) {
+        console.log(node)
+        this.setState({toolTipText: node.name})
+        this.showTooltip()
+    }
+
+    onMouseOut() {
+        this.hideTooltip()
+    }
+
+    showTooltip() {
+        console.log("show tooltip")
+        this.setState({isTooltipActive: true})
+        console.log(" tooltip" + this.state.isTooltipActive)
+    }
+
+    hideTooltip() {
+        this.setState({isTooltipActive: false})
     }
 
     render() {
@@ -40,18 +71,27 @@ class DependencyGraph extends Component {
         if (isEmptyObject(this.props.dependencies)) {
             graph = ""
         } else {
-            graph = <Tree data={[this.props.dependencies]}
+            graph = <Tree id="treeData" data={[this.props.dependencies]}
                           nodeSvgShape={svgSquare}
                           initialDepth={0}
                           pathFunc={"diagonal"}
                           depthFactor={300}
                           translate={this.state.translate}
+                          onMouseOver={this.onMouseOver}
+                //onMouseOut={this.onMouseOut}
             />
         }
 
         return (
-            <div style={containerStyles} ref={tc => (this.treeContainer = tc)}>
-                {graph}
+            <div>
+                <div style={tooltipStyles}>
+                    <ToolTip active={this.state.isTooltipActive} position="top" arrow="center" parent="#treeData">
+                        {this.state.toolTipText}
+                    </ToolTip>
+                </div>
+                <div style={containerStyles} ref={tc => (this.treeContainer = tc)}>
+                    {graph}
+                </div>
             </div>
         )
     }
